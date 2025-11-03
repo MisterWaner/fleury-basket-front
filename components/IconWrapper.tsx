b6492ce1baf7183ed018102c8f4fc8b50d1fcff3
Erwan Soulas <erwan.soulas@gmail.com>
2025-11-03 22:26:58 +0100

    7th commit: --FINISH ValorSection for Homepage.

---
+++ components/IconWrapper.tsx
@@ -0,0 +1,12 @@
+interface IconWrapperProps {
+    children: React.ReactNode;
+}
+export default function IconWrapper({ children }: IconWrapperProps) {
+    return (
+        <div className="group relative flex items-center justify-center w-20 h-20 rounded-full overflow-hidden border-2 border-slate-950  bg-slate-950 text-slate-50 hover:text-slate-950 transition-colors duration-400">
+            <span className="absolute inset-0 rounded-full scale-0 bg-slate-50 opacity-0 transition-transform duration-400 ease-out group-hover:scale-150 group-hover:opacity-100"></span>
+            <span className="relative z-10">{children}</span>
+        </div>
+    );
+}
+
--- components/ValorSection.tsx
+++ components/ValorSection.tsx
@@ -1,5 +1,6 @@
 import { GiBasketballBasket, GiBasketballJersey } from "react-icons/gi";
 import { PiStrategy } from "react-icons/pi";
+import IconWrapper from "./IconWrapper";
 
 export default function ValorSection() {
     return (
@@ -7,15 +8,48 @@ export default function ValorSection() {
             <h2 className="text-center text-3xl sm:text-4xl md:text-5xl mb-8">
                 Nos <span className="uppercase font-extrabold">Principes</span>
             </h2>
-            <div className="w-full flex-col justify-center items-center">
-                <article>
-                    <GiBasketballBasket size={48} />
+            <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8">
+                <article className="p-6">
+                    <div className="flex justify-center mb-4">
+                        <IconWrapper>
+                            <GiBasketballBasket size={56} />
+                        </IconWrapper>
+                    </div>
+                    <h3 className="text-2xl font-bold text-center">Découverte</h3>
+                    <p className="mt-4 text-sm text-center">
+                        Venez découvrir le basket, que ce soit en compétition ou
+                        en loisir, vous pouvez tester jusqu&apos;à 3 Séances
+                        offertes jusqu&apos;au 31 Octobre de l&apos;année en
+                        cours.
+                    </p>
                 </article>
-                <article>
-                    <PiStrategy size={48} />
+                <article className="p-6">
+                    <div className="flex justify-center mb-4">
+                        <IconWrapper>
+                            <PiStrategy size={56} />
+                        </IconWrapper>
+                    </div>
+                    <h3 className="text-2xl font-bold text-center">Valeurs</h3>
+                    <div className="mt-4 text-sm text-center">
+                        Les valeurs que véhiculent notre club sont :
+                        <ul className="list-disc list-inside">
+                            <li>Le respect de chacun</li>
+                            <li>L&apos;engagement</li>
+                            <li>Le partage</li>
+                        </ul>
+                    </div>
                 </article>
-                <article>
-                    <GiBasketballJersey size={48} />
+                <article className="p-6">
+                    <div className="flex justify-center mb-4">
+                        <IconWrapper>
+                            <GiBasketballJersey size={56} />
+                        </IconWrapper>
+                    </div>
+                    <h3 className="text-2xl font-bold text-center">Bénévolat</h3>
+                    <p className="mt-4 text-sm text-center">
+                        Les principaux acteurs du club sont bénévoles actifs:
+                        dirigeants, coachs, arbitres et OTMs.
+                    </p>
                 </article>
             </div>
         </div>
